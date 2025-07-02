0af8851616aec8e8bc5da486717a64320730ac56
TomBurnettUK <t_burnett@outlook.com>
2025-07-02 14:41:48 +0100

    Initial commit

---
+++ dist/repl.js
@@ -0,0 +1,22 @@
+import { createInterface } from "node:readline";
+export function cleanInput(input) {
+    return input
+        .toLowerCase()
+        .split(" ")
+        .filter((s) => s);
+}
+export function startREPL() {
+    const readline = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+    readline.prompt();
+    readline.on("line", (line) => {
+        const words = cleanInput(line);
+        if (words.length > 0) {
+            console.log("Your command was: " + words[0]);
+        }
+        readline.prompt();
+    });
+}
---
+++ dist/repl.test.js
@@ -0,0 +1,28 @@
+import { cleanInput } from "src/repl";
+import { describe, expect, test } from "vitest";
+describe.each([
+    {
+        input: "  hello world  ",
+        expected: ["hello", "world"],
+    },
+    {
+        input: " hello world  again ",
+        expected: ["hello", "world", "again"],
+    },
+    {
+        input: "hELlo worLd",
+        expected: ["hello", "world"],
+    },
+    {
+        input: "  hello  ",
+        expected: ["hello"],
+    },
+])("cleanInput($input)", ({ input, expected }) => {
+    test(`Expected: ${expected}`, () => {
+        const actual = cleanInput(input);
+        expect(actual).toHaveLength(expected.length);
+        for (const i in expected) {
+            expect(actual[i]).toBe(expected[i]);
+        }
+    });
+});
